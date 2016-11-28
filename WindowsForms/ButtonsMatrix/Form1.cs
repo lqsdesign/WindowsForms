527d35ab0a8c2ea0dedbc45dd5f4690c0217f221
lqs <lqsdesign@gmail.com>
2016-11-28 17:29:40 +0100

    add ButtonsMatrix task

---
+++ WindowsForms/ButtonsMatrix/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ButtonsMatrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            int k = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    Button button = new Button();
+                    button.Parent = this;
+                    button.Left = (i * 100) - 100;
+                    button.Top = (j * 100) - 100;
+                    button.Width = 100;
+                    button.Height = 100;
+                    button.Text = k.ToString();
+                    
+                    if ((i + j) % 2 == 0)
+                    {
+                        button.BackColor = Color.Black;
+                        button.ForeColor = Color.Wheat;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+}
--- WindowsForms/PictureBox/PictureBox.Designer.cs
+++ WindowsForms/PictureBox/PictureBox.Designer.cs
@@ -69,27 +69,27 @@
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.openToolStripMenuItem.Text = "&Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // printToolStripMenuItem
             // 
             this.printToolStripMenuItem.Name = "printToolStripMenuItem";
-            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.printToolStripMenuItem.Text = "&Print";
             this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(143, 6);
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
             this.closeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.closeToolStripMenuItem.Text = "&Close";
             this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
             // 
